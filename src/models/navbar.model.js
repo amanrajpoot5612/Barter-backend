1ea0b2a2e67fcff24b56580d1cdd6d04b40141e0
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-16 15:41:18 +0530

    Refactor navbar model to import NavbarItemSchema from separate file

--- src/models/navbar.model.js
+++ src/models/navbar.model.js
@@ -1,29 +1,5 @@
 import mongoose from "mongoose";
-
-const NavbarItemSchema = new mongoose.Schema(
-    {
-        label: {
-            type: String,
-            required: true
-        },
-        key:{
-            type: String,
-            trim: true,
-            lowercase: true,
-            required: true
-        },
-        link:{
-            type: String,
-            // requred: true
-        },
-        icon: {
-            type: String
-        }
-    },
-    {
-        timestamps: true,
-    }
-)
+import NavbarItemSchema from './Navbar.item.model.js'
 
 const NavbarSchema = new mongoose.Schema(
     {
